d59e798b484702a1a1dad31b2dd5eeb44fb5b03f
Renan Marcel Paulino leite <renan.leite@globo.com>
2018-05-02 02:09:21 -0300

    Feat: Implementação da pasta de consulta

--- dew.site/Pages/Administrador/Usuarios/Consulta.cshtml.cs
+++ dew.site/Pages/Administrador/Usuarios/Consulta.cshtml.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using dew.site.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace dew.site.Pages.Administrador.Usuarios
-{
-    public class ConsultaModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace dew.site.Pages.Administrador.Usuarios {
+    public class ConsultaModel : PageModel {
+        private readonly ApplicationDbContext applicationDbContext;
 
+        public ConsultaModel (ApplicationDbContext _applicationDbContext) {
+            applicationDbContext = _applicationDbContext;
+        }
+        public void OnGet () {
+           ViewData["Users"] = applicationDbContext.Users.ToList();
         }
 
-        public IActionResult OnPost()
-        {
-            return Page();
+        public IActionResult OnPost () {
+            return Page ();
         }
     }
 }
