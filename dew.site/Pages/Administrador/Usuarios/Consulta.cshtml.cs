bd79500531682bf8286a8895bf79cbf1a0dec477
Renan Marcel Paulino Leite <renan.leite@wllm.com.br>
2018-05-02 14:49:38 -0300

    fix: Grid de Usuarios

--- dew.site/Pages/Administrador/Usuarios/Consulta.cshtml.cs
+++ dew.site/Pages/Administrador/Usuarios/Consulta.cshtml.cs
@@ -6,17 +6,27 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace dew.site.Pages.Administrador.Usuarios {
     public class ConsultaModel : PageModel {
+
         private readonly ApplicationDbContext applicationDbContext;
 
         public ConsultaModel (ApplicationDbContext _applicationDbContext) {
             applicationDbContext = _applicationDbContext;
+            Consulta = new SearchModel();
         }
+        
+        public SearchModel Consulta { get; set; }
+        
         public void OnGet () {
-           ViewData["Users"] = applicationDbContext.Users.ToList();
+            Consulta.Users = applicationDbContext.Users.ToList();
         }
-
+        
         public IActionResult OnPost () {
             return Page ();
+        }
+        
+        public class SearchModel
+        {
+            public List<ApplicationUser> Users { get; set; }
         }
     }
 }
